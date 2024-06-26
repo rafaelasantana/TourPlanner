d5c815a0caa8532a56289147ebc98882afe45660
Roger Leite <rogeraleite@gmail.com>
2024-06-26 10:52:48 +0200

    improving tour details, map size and time format

--- TourPlanner/Services/TimeSpanExtensions.cs
+++ TourPlanner/Services/TimeSpanExtensions.cs
@@ -1,9 +1,24 @@
-﻿namespace TourPlanner.Services;
-
-public static class TimeSpanExtensions
+﻿namespace TourPlanner.Services
 {
-    public static string Format(this TimeSpan timeSpan)
+    public static class TimeSpanExtensions
     {
-        return $"{timeSpan.Days} day(s) {timeSpan.Hours} hour(s) {timeSpan.Minutes} minute(s) {timeSpan.Seconds} second(s)";
+        public static string Format(this TimeSpan timeSpan)
+        {
+            var parts = new List<string>();
+
+            if (timeSpan.Days > 0)
+                parts.Add($"{timeSpan.Days} day(s)");
+
+            if (timeSpan.Hours > 0)
+                parts.Add($"{timeSpan.Hours} hour(s)");
+
+            if (timeSpan.Minutes > 0)
+                parts.Add($"{timeSpan.Minutes} minute(s)");
+
+            if (timeSpan.Seconds > 0)
+                parts.Add($"{timeSpan.Seconds} second(s)");
+
+            return string.Join(" ", parts);
+        }
     }
 }
